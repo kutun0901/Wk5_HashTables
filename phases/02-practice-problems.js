332a36d0a91b6068979ce0b9d1685f879cb1983e
Sean Baeyens <srbaeyens@gmail.com>
2022-11-28 18:15:55 -0600

    solved sum

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -29,7 +29,17 @@ function duplicate(arr) {
 
 
 function twoSum(nums, target) {
-  // Your code here
+  // let total = 0
+  let set = new Set()
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(target - nums[i])) {
+      return true
+    } else {
+      set.add(nums[i])
+    }
+  }
+  return false
+
 }
 
 
